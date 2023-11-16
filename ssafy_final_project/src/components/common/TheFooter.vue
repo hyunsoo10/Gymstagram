08e51199c807986126148a83e3f0a7e7cefca00c
se2develop <seahee0130@gmail.com>
2023-11-17 00:54:26 +0900

    Design : 프로필 디자인 수정 완료

---
+++ ssafy_final_project/src/components/common/TheFooter.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer>
+        <div>
+        footer 내용 채울 공간임!
+        <br>
+        <a href="/">ssafy@ssafy.com</a>
+    </div>
+    </footer>
+</template>
+ 
+<script setup>
+
+</script>
+ 
+<style scoped>
+footer {
+    border-top: 1px solid rgb(172, 172, 172);
+    display: flex;
+    height: 100px;
+    text-align: center;
+    padding: 3px;
+    background-color: #ffffff;
+    color: rgb(172, 172, 172);
+    justify-content: center;
+    align-items: center;
+}
+
+footer a {
+    text-decoration-line: none;
+}
+</style>
